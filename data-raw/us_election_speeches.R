9d674fd378b155f3bcc4d8cb71497b314c78dcad
mcnakhaee <mcnakhaee@gmail.org>
2020-10-24 17:17:15 +0200

    add new version

--- data-raw/us_election_speeches.R
+++ data-raw/us_election_speeches.R
@@ -1,8 +1,8 @@
 ## code to prepare `us_election_speeches` dataset goes here
 
-library(tidyverse)
+library(readr)
 
 #us_election_speeches <- read_delim('data-raw/us_2020_election_speeches.tsv',delim = '\t')
 
-us_election_speeches <- read_delim('data-raw/all_transcripts_24_10_2020.csv',delim = '\t')
+us_election_speeches <- read_delim('data-raw/us_2020_election_speeches_24_10_2010.tsv',delim = '\t')
 usethis::use_data(us_election_speeches, overwrite = TRUE)
