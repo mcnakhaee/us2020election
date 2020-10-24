5b0c3c48d5355376751fbfc11c47d6594acf1aff
mcnakhaee <mcnakhaee@gmail.org>
2020-10-24 12:53:54 +0200

    update data

--- data-raw/us_election_speeches.R
+++ data-raw/us_election_speeches.R
@@ -2,5 +2,7 @@
 
 library(tidyverse)
 
-us_election_speeches <- read_delim('data-raw/us_2020_election_speeches.tsv',delim = '\t')
+#us_election_speeches <- read_delim('data-raw/us_2020_election_speeches.tsv',delim = '\t')
+
+us_election_speeches <- read_delim('data-raw/all_transcripts_24_10_2020.csv',delim = '\t')
 usethis::use_data(us_election_speeches, overwrite = TRUE)
